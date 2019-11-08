f352188e3d16cce7aebaa3a262dc83de318c7621
L3n1ad <danieltoth0910@gmail.com>
2019-11-08 19:44:28 +0000

    test check_quantity_low function in item class

---
+++ models/specs/item_spec.rb
@@ -0,0 +1,27 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../item.rb')
+
+class ItemTest < MiniTest::Test
+
+  def setup
+    @item1 = Item.new('book_id' => 1, 'quantity' => 1, 'buying_cost' => 4.50, 'selling_price' => 7.75)
+    @item2 = Item.new('book_id' => 2, 'quantity' => 5, 'buying_cost' => 4.50, 'selling_price' => 7.75)
+    @item3 = Item.new('book_id' => 3, 'quantity' => 10, 'buying_cost' => 4.50, 'selling_price' => 7.75)
+  end
+
+  def test_check_quantity_low__under_limit
+    result = @item1.check_quantity_low
+    assert_equal(true, result)
+  end
+
+  def test_check_quantity_low__on_limit
+    result = @item2.check_quantity_low
+    assert_equal(false, result)
+  end
+  def test_check_quantity_low__over_limit
+    result = @item3.check_quantity_low
+    assert_equal(false, result)
+  end
+
+end
