8b89c692e4a50d165d86a0bb4e4df710898e09cb
L3n1ad <danieltoth0910@gmail.com>
2019-11-08 17:20:43 +0000

    create save, delete_all, find_all for author class and create file structure for all the classes

---
+++ models/author.rb
@@ -0,0 +1,38 @@
+require_relative('../db/sqlrunner.rb')
+
+class Author
+
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :date_of_birt, :description
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @first_name = options['first_name']
+    @last_name = options['last_name']
+    @date_of_birt = options['date_of_birt'].to_i
+    @description = options['description']
+  end
+
+  def save()
+    sql = 'INSERT INTO authors(first_name,last_name,date_of_birt,description)
+          VALUES ($1, $2, $3, $4)
+          RETURNING id'
+    values = [@first_name, @last_name, @date_of_birt, @description]
+    result = SqlRunner.run(sql, values).first
+    @id = result['id']
+  end
+
+
+  def self.all()
+    sql = 'SELECT * FROM authors;'
+    result = SqlRunner.run(sql)
+    return result.map{|author| Author.new(author)}
+  end
+
+  def self.delete_all()
+    sql = 'DELETE FROM authors'
+    SqlRunner.run(sql)
+  end
+
+
+end
