7efc0c682cffa6ff8782893d77d17602b90f852d
L3n1ad <danieltoth0910@gmail.com>
2019-11-10 14:38:43 +0000

    added stock_controller path to app.rb

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require('sinatra/contrib/all')
 require_relative('controllers/book_controller.rb')
 require_relative('controllers/author_controller.rb')
 require_relative('controllers/publication_controller.rb')
+require_relative('controllers/stock_controller.rb')
 
 get '/' do
   erb(:index)
--- db/seed.rb
+++ db/seed.rb
@@ -40,8 +40,8 @@ publication2.save
 publication3.save
 publication4.save
 
-item1 = Item.new('book_id' => book1.id, 'quantity' => 1, 'buying_cost' => 4.50, 'selling_price' => 7.75)
-item2 = Item.new('book_id' => book2.id, 'quantity' => 5, 'buying_cost' => 4.50, 'selling_price' => 7.75)
+item1 = Item.new('book_id' => book1.id, 'quantity' => 1, 'buying_cost' => 4.509, 'selling_price' => 7.75)
+item2 = Item.new('book_id' => book2.id, 'quantity' => 5, 'buying_cost' => 4.5, 'selling_price' => 7.75)
 item3 = Item.new('book_id' => book3.id, 'quantity' => 10, 'buying_cost' => 4.50, 'selling_price' => 7.75)
 
 item1.save
