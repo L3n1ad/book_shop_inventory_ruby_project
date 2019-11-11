aab38ae54537f77a4b2df16ce4a9eeb5fec0a9dc
L3n1ad <danieltoth0910@gmail.com>
2019-11-11 13:16:27 +0000

    add post route with all the data for filtering by author

--- controllers/book_controller.rb
+++ controllers/book_controller.rb
@@ -1,4 +1,4 @@
-require('sinatra')
+ require('sinatra')
 require('sinatra/contrib/all')
 require('pry-byebug')
 require_relative('../models/book.rb')
--- controllers/stock_controller.rb
+++ controllers/stock_controller.rb
@@ -9,5 +9,12 @@ also_reload('../models/*')
 
 get '/stock' do
   @sorted_by_genre = Item.sort_by_genre
+  @authors = Author.all
   erb(:"stock/index")
+end
+
+post '/stock' do
+  @author_id = params[:author_id]
+  @publications = Publication.sort_by_author[@author_id.to_i]
+  erb(:"stock/sorted_by_author")
 end
