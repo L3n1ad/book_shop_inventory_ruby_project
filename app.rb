02fe40ad01fa3d358d3acb566a4a5102564ace0c
L3n1ad <danieltoth0910@gmail.com>
2019-11-12 12:33:48 +0000

    fixed models reload

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require_relative('controllers/book_controller.rb')
 require_relative('controllers/author_controller.rb')
 require_relative('controllers/publication_controller.rb')
 require_relative('controllers/stock_controller.rb')
-also_reload('../models/*')
+also_reload('./models/*')
 
 get '/' do
   erb(:index)
